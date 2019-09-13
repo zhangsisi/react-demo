c9b2d04df55d59d9c298f8aab0f6e63ddad01f4c
zhangsisi <ssyy4411@163.com>
2019-09-13 16:21:25 +0800

    redux

--- src/pages/welcome/test01.js
+++ src/pages/welcome/test01.js
@@ -1,5 +1,14 @@
 import React, { Component } from 'react';
-
+init()
+function A () {
+  console.log('a')
+}
+function B () {
+  console.log('b')
+}
+function init () {
+  A.call(B)
+}
 class App extends Component {
   componentDidMount () {
     this.init()
